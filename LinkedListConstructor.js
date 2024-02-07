068c7d6d6e5fa15a0ba965cbf5591f398b0269bd
100ngram <sangramjitdutta@gmail.com>
2024-02-07 21:34:16 +0530

    Pointers and Linked List constructor

---
+++ LinkedListConstructor.js
@@ -0,0 +1,18 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    const NewNode = new Node(value);
+    this.head = NewNode;
+    this.length = 1;
+    this.tail = this.head;
+  }
+}
+
+const LL1 = new LinkedList(4);
+console.log(LL1);
---
+++ cookieClass.js
@@ -0,0 +1,7 @@
+var myObj1 = {
+  name: "boo",
+};
+var myObj2 = myObj1;
+
+myObj1.name = "cooldude";
+console.log(myObj2);
