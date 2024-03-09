4619bdf78246996e02d81405a78e35b5272383c1
100ngram <sangramjitdutta@gmail.com>
2024-03-09 20:47:07 +0530

    Queue : added constructor and node

---
+++ Queue.js
@@ -0,0 +1,20 @@
+//node class
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.first = newNode;
+    this.last = newNode;
+    this.length = 1;
+  }
+}
+
+const myQueue = new Queue(25);
+console.log(myQueue);
