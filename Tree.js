c73b3d34670dd96b03c14e82762ab28feea46825
100ngram <sangramjitdutta@gmail.com>
2024-03-12 17:10:28 +0530

    BST : constructor

--- Tree.js
+++ Tree.js
@@ -5,3 +5,13 @@ class Node {
     this.right = null;
   }
 }
+
+class BST {
+  constructor(value) {
+    const rootNode = new Node(value);
+    this.root = rootNode;
+  }
+}
+
+let myBST = new BST(256);
+console.log(myBST);
