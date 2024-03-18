e04406d9454ea2ad6d21f345850cb59a65de71d1
100ngram <sangramjitdutta@gmail.com>
2024-03-18 18:40:36 +0530

    BST : insert method

--- Tree.js
+++ Tree.js
@@ -11,7 +11,37 @@ class BST {
     const rootNode = new Node(value);
     this.root = rootNode;
   }
+
+  //INSERT (insert a node in the BST)
+
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+    let temp = this.root;
+    while (true) {
+      if (newNode.value === temp.value) return undefined;
+      if (newNode.value < temp.value) {
+        if (temp.left === null) {
+          temp.left = newNode;
+          return this;
+        }
+        temp = temp.left;
+      } else {
+        if (temp.right === null) {
+          temp.right = newNode;
+          return this;
+        }
+        temp = temp.right;
+      }
+    }
+  }
 }
 
-let myBST = new BST(256);
+let myBST = new BST(25);
+myBST.insert(6);
+myBST.insert(44);
+myBST.insert(78);
 console.log(myBST);
